b95238ba089e5c841fcede765dfdbcf86b72f2e4
Ashith V L <ashithvl@gmail.com>
2022-08-05 07:34:10 +0530

    1._StateLessReader

--- src/main/java/com/codeWithAshith/SpringBatch/SpringBatchApplication.java
+++ src/main/java/com/codeWithAshith/SpringBatch/SpringBatchApplication.java
@@ -1,9 +1,14 @@
 package com.codeWithAshith.SpringBatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/codeWithAshith/SpringBatch/configuration/JobConfiguration.java
+++ src/main/java/com/codeWithAshith/SpringBatch/configuration/JobConfiguration.java
@@ -8,10 +8,12 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Configuration
 public class JobConfiguration {
 
     @Autowired
