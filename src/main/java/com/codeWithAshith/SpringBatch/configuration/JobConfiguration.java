8415f058e573d94ce31fa8b23d53702102917389
Ashith V L <ashithvl@gmail.com>
2022-08-05 08:33:57 +0530

    3._Reading_From_csv

--- src/main/java/com/codeWithAshith/SpringBatch/configuration/JobConfiguration.java
+++ src/main/java/com/codeWithAshith/SpringBatch/configuration/JobConfiguration.java
@@ -1,17 +1,20 @@
 package com.codeWithAshith.SpringBatch.configuration;
 
 
-import com.codeWithAshith.SpringBatch.data.Person;
-import com.codeWithAshith.SpringBatch.data.PersonRowMapper;
+import com.codeWithAshith.SpringBatch.data.Employee;
+import com.codeWithAshith.SpringBatch.data.EmployeeMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -25,26 +28,34 @@ public class JobConfiguration {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
-    @Autowired
-    private DataSource dataSource;
     @Bean
-    public JdbcCursorItemReader<Person> cursorItemReader(){
-        JdbcCursorItemReader<Person> reader = new JdbcCursorItemReader<Person>();
+    public FlatFileItemReader<Employee> cursorItemReader() {
+        //Create reader instance
+        FlatFileItemReader<Employee> reader = new FlatFileItemReader<Employee>();
+        reader.setResource(new ClassPathResource("employee.csv"));
+        reader.setLinesToSkip(1);
 
-        reader.setSql("SELECT id, name, location, birth_date FROM person");
-        reader.setDataSource(dataSource);
-        reader.setRowMapper(new PersonRowMapper());
+        DefaultLineMapper<Employee> lineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames(new String[] { "id", "firstName", "lastName" });
+
+        lineMapper.setLineTokenizer(tokenizer);
+        lineMapper.setFieldSetMapper(new EmployeeMapper());
+        lineMapper.afterPropertiesSet();
+
+        reader.setLineMapper(lineMapper);
 
         return reader;
     }
 
     @Bean
-    public ItemWriter<Person> personItemWriter(){
-        return new ItemWriter<Person>() {
+    public ItemWriter<Employee> personItemWriter() {
+        return new ItemWriter<Employee>() {
             @Override
-            public void write(List<? extends Person> items) throws Exception {
-                for(Person person: items){
-                    System.out.println(person.toString());
+            public void write(List<? extends Employee> items) throws Exception {
+                for (Employee employee : items) {
+                    System.out.println(employee.toString());
                 }
             }
         };
@@ -53,14 +64,14 @@ public class JobConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<Person, Person>chunk(10)
+                .<Employee, Employee>chunk(10)
                 .reader(cursorItemReader())
                 .writer(personItemWriter())
                 .build();
     }
 
     @Bean
-    public Job interfaceJob(){
+    public Job interfaceJob() {
         return jobBuilderFactory.get("interfaceJob")
                 .start(step1())
                 .build();
